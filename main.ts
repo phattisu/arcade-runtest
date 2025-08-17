96f948e324b513e79dc287dbe7f6c5212c53791c
Phattisu <naphatism@hotmail.com>
2025-08-17 16:03:28 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,56 @@
 
+enum ValueFormat {
+    //% block="intenger"
+    int = 0,
+    //% block="float"
+    float = 1,
+}
+
 //% block="run test" color="#7f7e8c" icon="\uf121"
-namespace runtest {
+namespace RunTest {
     let runResult = 0
 
     //% blockId=runtest_init
     //% block="on run testing do"
     //% handlerStatement
-    //% weight=3
-    export function init(mystat: () => void) {
+    //% weight=20
+    export function initRun(mystate: () => void) {
         const time = control.millis()
-        mystat()
+        mystate()
         runResult = control.millis() - time
     }
 
     //% blockId=runtest_result
-    //% block="runtest result (millis)"
-    //% weight=2
-    export function result() {
-        return runResult
+    //% block="runtest result (millis) in $resultType"
+    //% weight=19
+    export function result(resultType: ValueFormat) {
+        switch (resultType) {
+            case 0: return runResult
+            case 1: return runResult / 1000
+        }
+        return NaN
     }
 
     //% blockId=runtest_runtime
-    //% block="system runtime (millis)"
-    //% weight=1
-    export function sysRuntimeMS() {
-        return control.millis()
+    //% block="system runtime (millis) in $resultType"
+    //% weight=18
+    export function sysRuntimeMs(resultType: ValueFormat) {
+        switch (resultType) {
+            case 0: return control.millis()
+            case 1: return control.millis() / 1000
+        }
+        return NaN
+    }
+
+    //% blockId=runtest_deltatime
+    //% block="delta time (millis) in $resultType"
+    //% weight=17
+    export function deltaMs(resultType: ValueFormat) {
+        switch (resultType) {
+            case 0: return control.eventContext().deltaTimeMillis
+            case 1: return control.eventContext().deltaTime
+        }
+        return NaN
     }
 
 }
